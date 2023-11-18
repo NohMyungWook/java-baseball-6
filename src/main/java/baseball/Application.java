7c9cf699573a0be2187c189a38197314b6f5e70a
noah <jeongas9810@gmail.com>
2023-11-18 16:58:32 +0900

    feat: Add Console.close in Application

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,12 @@
 package baseball;
 
 import baseball.common.BaseballGame;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
         BaseballGame game = new BaseballGame();
         game.start();
+        Console.close();
     }
 }
