3fa9235c2ca7b72efaaf0b343bd488c912ea865e
noah <jeongas9810@gmail.com>
2023-11-18 16:32:44 +0900

    feat: Change the logic of receiving input from the user and generating a list.

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -4,10 +4,19 @@ import baseball.util.Parser;
 import baseball.view.constant.ViewMessage;
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class InputView {
+    private static final String SEPARATOR = "";
+
     private static final Parser parser = new Parser();
-    public int readNumber() {
+
+    public List<Integer> readNumber() {
         ViewMessage.ASK_NUMBER.print();
-        return parser.parseNumber(Console.readLine());
+
+        return Arrays.stream(Console.readLine().split(SEPARATOR))
+                .mapToInt(parser::parseNumber)
+                .boxed().toList();
     }
 }
