3ab7174892bf71f52cff3d360a6f6bb678aee123
noah <jeongas9810@gmail.com>
2023-11-18 16:59:27 +0900

    style: Modify Error Message

--- src/main/java/baseball/view/constant/ErrorMessage.java
+++ src/main/java/baseball/view/constant/ErrorMessage.java
@@ -3,7 +3,7 @@ package baseball.view.constant;
 public enum ErrorMessage implements Message {
     NOT_A_NUMBER("숫자가 아닙니다."),
     INVALID_INPUT("올바른 입력값이 아닙니다."),
-    INVALID_INPUT_LENGTH("3자리가 아닙니다.")
+    INVALID_INPUT_LENGTH("3자리의 숫자가 아닙니다.")
     ;
 
     private final String errorMessage;
