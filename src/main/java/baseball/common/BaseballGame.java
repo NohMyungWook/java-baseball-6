461e6270379a9d77877f4cc27297d89585e489e9
noah <jeongas9810@gmail.com>
2023-11-18 16:33:51 +0900

    feat: Change start method.
    1. print welcome message in initializeGameSetting
    2. make numberBaseball object through inputView.

--- src/main/java/baseball/common/BaseballGame.java
+++ src/main/java/baseball/common/BaseballGame.java
@@ -3,17 +3,21 @@ package baseball.common;
 import baseball.model.NumberBaseball;
 import baseball.service.NumberMachine;
 import baseball.view.InputView;
+import baseball.view.OutputView;
 
 public class BaseballGame {
     private static final NumberMachine numberMachine = new NumberMachine();
     private static final InputView inputView = new InputView();
+    private static final OutputView outputView = new OutputView();
 
     public void start() {
         NumberBaseball computer = initializeGameSetting();
-        inputView.readNumber();
+        NumberBaseball user = new NumberBaseball(inputView.readNumber());
+
     }
 
     private NumberBaseball initializeGameSetting() {
+        outputView.printStartMessage();
         return numberMachine.makeAnswer();
     }
 }
---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,9 @@
+package baseball.view;
+
+import baseball.view.constant.ViewMessage;
+
+public class OutputView {
+    public void printStartMessage() {
+        ViewMessage.START.print();
+    }
+}
