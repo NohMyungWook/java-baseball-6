a32863a572579baf204048bcd8145d65156b1f9a
noah <jeongas9810@gmail.com>
2023-11-20 18:53:41 +0900

    feat: Add the ability to play until you get the correct answer

--- src/main/java/baseball/common/BaseballGame.java
+++ src/main/java/baseball/common/BaseballGame.java
@@ -1,7 +1,10 @@
 package baseball.common;
 
+import baseball.model.BaseballResult;
 import baseball.model.NumberBaseball;
 import baseball.service.NumberMachine;
+import baseball.service.ResultMachine;
+import baseball.service.constant.MachineConstant;
 import baseball.view.InputView;
 import baseball.view.OutputView;
 
@@ -13,13 +16,22 @@ public class BaseballGame {
     public void start() {
         NumberBaseball computer = initializeGameSetting();
         do {
-            NumberBaseball user = new NumberBaseball(inputView.readNumber());
+            playGame(computer);
         } while (wantToContinue());
     }
 
     private NumberBaseball initializeGameSetting() {
         outputView.printStartMessage();
         return numberMachine.makeAnswer();
+    }
+
+    private void playGame(NumberBaseball computer) {
+        BaseballResult baseballResult;
+        do {
+            NumberBaseball user = new NumberBaseball(inputView.readNumber());
+            ResultMachine resultMachine = new ResultMachine(computer, user);
+            baseballResult = resultMachine.createBaseballResult();
+        } while (baseballResult.strike() != MachineConstant.MAX_STRIKE);
     }
 
     private boolean wantToContinue() {
