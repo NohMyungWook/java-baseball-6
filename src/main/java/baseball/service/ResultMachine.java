360468b3f885ddb0eef929d6df1a01b5bbcbd19d
noah <jeongas9810@gmail.com>
2023-11-20 18:53:09 +0900

    feat: Add functionality for calculating game results
    1. Adding a constant-only class containing existing constants.
    2. Adding a compute class that takes the list as a factor.
    3. Count the number of balls and strikes in the calculation class.

---
+++ src/main/java/baseball/service/ResultMachine.java
@@ -0,0 +1,38 @@
+package baseball.service;
+
+import baseball.model.BaseballResult;
+import baseball.model.NumberBaseball;
+import baseball.service.constant.MachineConstant;
+
+import java.util.Objects;
+import java.util.stream.IntStream;
+
+public class ResultMachine {
+    private final NumberBaseball computer;
+    private final NumberBaseball user;
+
+    public ResultMachine(NumberBaseball computer, NumberBaseball user) {
+        this.computer = computer;
+        this.user = user;
+    }
+
+    public BaseballResult createBaseballResult() {
+        int strike = (int) IntStream.range(MachineConstant.INITIALIZE_INDEX, MachineConstant.MAX_SIZE)
+                .filter(this::isStrike)
+                .count();
+        int ball = (int) IntStream.range(MachineConstant.INITIALIZE_INDEX, MachineConstant.MAX_SIZE)
+                .filter(index -> !isStrike(index) && isBall(index))
+                .count();
+
+        return new BaseballResult(ball, strike);
+    }
+
+    private boolean isStrike(int index) {
+        return Objects.equals(this.computer.getNumbers().get(index), this.user.getNumbers().get(index));
+    }
+
+    private boolean isBall(int index) {
+        return this.computer.getNumbers().contains(this.user.getNumbers().get(index));
+    }
+
+}
---
+++ src/main/java/baseball/service/constant/MachineConstant.java
@@ -0,0 +1,9 @@
+package baseball.service.constant;
+
+public class MachineConstant {
+    public static final int INITIALIZE_INDEX = 0;
+    public static final int MAX_SIZE = 3;
+    public static final int MINIMUM_NUMBER = 1;
+    public static final int MAXIMUM_NUMBER = 9;
+    public static final int MAX_STRIKE = 3;
+}
